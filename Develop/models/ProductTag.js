f540de1cc95b21c54cae29de59cf9ee6d5adcbcb
StaishaKnight <staishaknight@gmail.com>
2022-04-05 19:35:13 -0500

    defined models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,13 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const { Tag, Product } = require('.');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
